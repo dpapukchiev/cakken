474e23aeff799698e76c20911be0a55d1572dfde
Daniel Papukchiev <daniel@studyportals.com>
2018-10-16 10:47:34 +0200

    Kinesis example

--- example/kinesis-transformer.ts
+++ example/kinesis-transformer.ts
@@ -1,19 +1,42 @@
 import { Chain, ItemIterator, SingleItemReducingComponent } from '../src';
 import { FirehoseTransformationResultHelper } from '../src/firehose-transformation-result-helper';
 
-class FirehoseChainComponent extends SingleItemReducingComponent<any, FirehoseTransformationResultHelper>{
+// Proceeds with elements with event keys
+class EvenKeyComponent extends SingleItemReducingComponent<any, FirehoseTransformationResultHelper>{
 
-    async handle(nextElement: any, reduced: FirehoseTransformationResultHelper): Promise<FirehoseTransformationResultHelper> {
+    async handle(nextElement: any, result: FirehoseTransformationResultHelper): Promise<FirehoseTransformationResultHelper> {
 
-        if (nextElement.key % 2 === 0) {
-
-            reduced.addOk(nextElement.id, nextElement.key, false);
-        } else {
-
-            reduced.addFailed(nextElement.id, nextElement.key, false);
-        }
-        return reduced;
+        result.addOk(nextElement.id, nextElement.key, false);
+        return result;
     }
+    canHandle(nextElement: any): boolean {
+
+        return nextElement.key % 2 === 0;
+    }
+}
+// Fails items with key === 2
+class TwoKeyFilterComponent extends SingleItemReducingComponent<any, FirehoseTransformationResultHelper>{
+
+    async handle(nextElement: any, result: FirehoseTransformationResultHelper): Promise<FirehoseTransformationResultHelper> {
+
+        result.addFailed(nextElement.id, nextElement.key, false);
+        return result;
+    }
+
+    canHandle(nextElement: any): boolean {
+
+        return nextElement.key === 2;
+    }
+}
+// All other items are failed
+class FailedSinkComponent extends SingleItemReducingComponent<any, FirehoseTransformationResultHelper>{
+
+    async handle(nextElement: any, result: FirehoseTransformationResultHelper): Promise<FirehoseTransformationResultHelper> {
+
+        result.addFailed(nextElement.id, nextElement.key, false);
+        return result;
+    }
+
     canHandle(nextElement: any): boolean {
 
         return true;
@@ -21,15 +44,18 @@ class FirehoseChainComponent extends SingleItemReducingComponent<any, FirehoseTr
 }
 
 const iterator = new ItemIterator<any>().setAll([
-    { key: 1, id: 1 },
-    { key: 2, id: 2 },
-    { key: 3, id: 3 },
-    { key: 4, id: 4 },
+    { key: 1, id: 1 }, // Fail - key not even
+    { key: 2, id: 2 }, // Fail - key = 2
+    { key: 3, id: 3 }, // Fail - key not event
+    { key: 4, id: 4 }, // OK - key even and != 2
+    { key: 6, id: 6 }, // OK - key even and != 2
 ]);
 
-// Even keys are added to the OK result, everything else fails
+// Even keys except 2 are added to the OK result, everything else fails
 const result = new Chain<any, FirehoseTransformationResultHelper>()
-    .registerComponent(new FirehoseChainComponent())
+    .registerComponent(new TwoKeyFilterComponent())
+    .registerComponent(new EvenKeyComponent())
+    .registerComponent(new FailedSinkComponent())
     .build()
     .run(new FirehoseTransformationResultHelper(), iterator);
 
